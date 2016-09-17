93169ced7ae515eb70de3a2e8ed5308bff7b20b5
Vinadoros <vinadoros@outlook.com>
2016-09-16 20:38:56 -0400

    Add p7zip.

--- Comp-FedoraSoftware.py
+++ Comp-FedoraSoftware.py
@@ -52,7 +52,7 @@ SOFTWARESCRIPT="""
 #!/bin/bash
 
 # Install cli tools
-dnf install -y fish tmux iotop rsync
+dnf install -y fish tmux iotop rsync p7zip p7zip-plugins
 
 # Management tools
 dnf install -y yumex-dnf gparted
