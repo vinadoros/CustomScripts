4787b5fbcad89bc154f7a467505ff8aa459c775a
Vinadoros <vinadoros@outlook.com>
2017-02-08 10:18:18 -0500

    Fix repos

--- Comp-OpensuseSoftware.py
+++ Comp-OpensuseSoftware.py
@@ -68,8 +68,11 @@ zypper ar -f -n packman http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_
 # Adobe Flash
 # https://en.opensuse.org/Adobe_Flash_Player
 zypper ar --check --refresh http://linuxdownload.adobe.com/linux/x86_64/ adobe
-zypper in -y adobe-release-x86_64
+rpm -ivh http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
+
+# Import all gpg keys
+zypper --non-interactive --gpg-auto-import-keys refresh
 
 """
 subprocess.run(REPOSCRIPT, shell=True)
@@ -85,7 +88,7 @@ zypper install -y gparted
 # Install browsers
 zypper install -y chromium MozillaFirefox freshplayerplugin
 # Adobe Flash
-zypper in -y flash-plugin
+zypper in -y flash-plugin flash-player-ppapi
 
 # Samba
 zypper install -y samba samba-client samba-winbind
@@ -106,6 +109,10 @@ zypper install -y pavucontrol paprefs vlc smplayer
 
 # terminator
 zypper install -y terminator
+
+# Change to NetworkManager
+systemctl disable wicked
+systemctl enable NetworkManager
 """
 # Install software for VMs
 if QEMUGUEST is True:
@@ -127,18 +134,23 @@ subprocess.run(SOFTWARESCRIPT, shell=True)
 if args.desktop is 1:
     DESKTOPSCRIPT="""
 # Gnome
-zypper install -y -t pattern gnome_admin gnome_basis gnome_basis_opt gnome_imaging gnome_utilities gnome_laptop gnome_yast eog sw_management_gnome
+zypper install -y -t pattern gnome_admin gnome_basis gnome_basis_opt gnome_imaging gnome_utilities gnome_laptop gnome_yast sw_management_gnome
+zypper install -y eog gnome-extension-terminal dconf-editor
 zypper install -y gdm
-# Some Gnome Extensions
-zypper install -y gnome-extension-terminal dconf-editor
 zypper install -y gnome-shell-extension-gpaste
+# Change display manager to gdm
+sed -i 's/DISPLAYMANAGER=.*$/DISPLAYMANAGER="gdm"/g' /etc/sysconfig/displaymanager
 """
 elif args.desktop is 3:
     DESKTOPSCRIPT="""
 # MATE
-zypper install -y -t pattern mate mate_admin mate_laptop mate_utilities
+zypper install -y -t pattern mate_basis mate_admin mate_laptop mate_utilities
 # Applications
 zypper install -y dconf-editor
+# Display Manager
+zypper install -y lightdm lightdm-gtk-greeter
+# Change display manager to lightdm
+sed -i 's/DISPLAYMANAGER=.*$/DISPLAYMANAGER="lightdm"/g' /etc/sysconfig/displaymanager
 """
 
 DESKTOPSCRIPT+="""
