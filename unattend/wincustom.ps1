32b8a71a2b9d939194ab2ff7425b1b6596dae42d
Vinadoros <vinadoros@outlook.com>
2017-04-08 21:50:26 -0400

    Do chocolatey first

--- unattend/wincustom.ps1
+++ unattend/wincustom.ps1
@@ -1,38 +1,19 @@
 $temppath = "C:\Windows\Temp"
 
-# Install 7-Zip
-echo "Installing 7-zip"
-$url = "http://www.7-zip.org/a/7z1604-x64.msi"
-$sevenfile = "$temppath\7z.msi"
-(New-Object System.Net.WebClient).DownloadFile($url, $sevenfile)
-Start-Process -Wait "$sevenfile" -ArgumentList "/qn","/passive"
-Remove-Item -Recurse -Force $sevenfile
-
-# Check if Windows 7
-if ([Environment]::OSVersion.Version.Major -eq 6 -And [Environment]::OSVersion.Version.Minor -eq 1){
-  echo "Windows 7 detected."
-  # Install .net Framework 4.5.2 on Windows 7
-  echo "Installing .net 4.5.2"
-  $url = "https://download.microsoft.com/download/9/A/7/9A78F13F-FD62-4F6D-AB6B-1803508A9F56/51209.34209.03/web/NDP452-KB2901954-Web.exe"
-  $netfile = "$temppath\NDP452-KB2901954-Web.exe"
-  (New-Object System.Net.WebClient).DownloadFile($url, $netfile)
-  Start-Process -Wait "$netfile" -ArgumentList /quiet
-  Remove-Item -Recurse -Force $netfile
-
-  # Check if Powershell is less than 4
-  if ($PSVersionTable.PSVersion.Major -lt 4){
-    # Install Powershell 5 on Windows 7
-    echo "Installing Powershell 5"
-    $url = "https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win7AndW2K8R2-KB3191566-x64.zip"
-    $psfile = "$temppath\Win7AndW2K8R2-KB3191566-x64.zip"
-    (New-Object System.Net.WebClient).DownloadFile($url, $psfile)
-    $psfolder = "$temppath\psfolder"
-    Start-Process -Wait "C:\Program Files\7-Zip\7z.exe" -ArgumentList "x","$psfile","-o$psfolder"
-    Start-Process -Wait "$psfolder\Win7AndW2K8R2-KB3191566-x64.msu" -ArgumentList /quiet,/norestart
-    Remove-Item -Recurse -Force $psfolder
-    Remove-Item -Recurse -Force $psfile
-  }
+# Chocolatey section
+echo "Installing Chocolatey"
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+echo "Installing Chocolatey packages"
+choco upgrade -y dotnet4.6.1 powershell
+choco upgrade -y googlechrome jre8 notepadplusplus git tortoisegit ccleaner putty chocolateygui conemu visualstudiocode winmerge libreoffice sumatrapdf pdfcreator 7zip
+# Install for Windows 8 or above.
+if ([Environment]::OSVersion.Version.Major -ge 8){
+  choco upgrade -y classic-shell
 }
+# Install for lower than Windows 8
+#if ([Environment]::OSVersion.Version.Major -lt 8){
+#  choco upgrade -y wincdemu
+#}
 
 # Get Ninite
 echo "Getting ninite"
@@ -62,20 +43,6 @@ if (Test-Path $winiso) {
   echo "Cleaning up VMTools"
   Remove-Item -Recurse -Force $winiso
   Remove-Item -Recurse -Force $vmfolder
-}
-
-# Chocolatey section
-echo "Installing Chocolatey"
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-echo "Installing Chocolatey packages"
-choco upgrade -y googlechrome jre8 notepadplusplus git tortoisegit ccleaner putty chocolateygui conemu visualstudiocode winmerge libreoffice sumatrapdf pdfcreator
-# Install for Windows 8 or above.
-if ([Environment]::OSVersion.Version.Major -ge 8){
-  choco upgrade -y classic-shell
-}
-# Install for lower than Windows 8
-if ([Environment]::OSVersion.Version.Major -lt 8){
-  choco upgrade -y wincdemu
 }
 
 # Windows customizations
