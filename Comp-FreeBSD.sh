48d4eb298e6775bb92ef2e7db74b2640e8e5dc3a
Vinadoros <vinadoros@outlook.com>
2017-05-18 09:43:39 -0400

    Add mate to freebsd

--- Comp-FreeBSD.sh
+++ Comp-FreeBSD.sh
@@ -4,9 +4,16 @@
 export ASSUME_ALWAYS_YES=yes
 
 # Update freebsd.
-freebsd-update fetch install
+freebsd-update -F fetch install
 # Update packages.
 pkg update -f
 
 # Install command line utilities.
 pkg install -y nano fish git
+
+# Install mate.
+pkg install -y xorg xf86-video-fbdev mate-desktop mate
+sysrc moused_enable=yes dbus_enable=yes hald_enable=yes
+# Slim display manager
+pkg install -y slim
+sysrc slim_enable=yes
