c1acf8afd5dd77f946769a4f18c82bd4a4b33687
Vinadoros <vinadoros@outlook.com>
2017-06-03 18:01:15 -0400

    Mod user detection

--- Yusbbackup_borg.sh
+++ Yusbbackup_borg.sh
@@ -11,18 +11,16 @@ echo "Executing ${SCRNAME}."
 # Add general functions if they don't exist.
 type -t grepadd &> /dev/null || source "$SCRIPTDIR/Comp-GeneralFunctions.sh"
 
-# Set user folders if they don't exist.
-if [ -z $USERNAMEVAR ]; then
-	if [[ ! -z "$SUDO_USER" && "$SUDO_USER" != "root" ]]; then
-		export USERNAMEVAR="$SUDO_USER"
-	elif [ "$USER" != "root" ]; then
-		export USERNAMEVAR="$USER"
-	else
-		export USERNAMEVAR="$(id 1000 -un)"
-	fi
-	USERGROUP="$(id 1000 -gn)"
-	USERHOME="/home/$USERNAMEVAR"
+# Set user folders.
+if [[ ! -z "$SUDO_USER" && "$SUDO_USER" != "root" ]]; then
+	export USERNAMEVAR=$SUDO_USER
+elif [ "$USER" != "root" ]; then
+	export USERNAMEVAR=$USER
+else
+	export USERNAMEVAR=$(id 1000 -un)
 fi
+export USERGROUP=$(id $USERNAMEVAR -gn)
+export USERHOME=/home/$USERNAMEVAR
 
 function usage()
 {
