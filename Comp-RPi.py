77bce3675b747196c4549d18b88e02a43a95c2aa
Vinadoros <vinadoros@outlook.com>
2017-06-15 15:10:55 -0400

    Add switches to rpi script

--- Comp-RPi.py
+++ Comp-RPi.py
@@ -14,6 +14,8 @@ import urllib.request
 
 # OmxPlayer from http://omxplayer.sconde.net/
 OMXURL = "http://omxplayer.sconde.net/builds/omxplayer_0.3.7~git20170130~62fb580_armhf.deb"
+# rpi-update from https://github.com/Hexxeh/rpi-update
+RPIUPDATEURL = "https://raw.githubusercontent.com/Hexxeh/rpi-update/master/rpi-update"
 
 print("Running {0}".format(__file__))
 print("This script is for use only on Ubuntu.")
@@ -24,6 +26,7 @@ parser.add_argument("-c", "--cliutils", help='Install command line utilities', a
 parser.add_argument("-d", "--docker", help='Install docker', action="store_true")
 parser.add_argument("-n", "--noprompt", help='Do not prompt to continue.', action="store_true")
 parser.add_argument("-o", "--omxdeb", help='Install OMXPlayer.', action="store_true")
+parser.add_argument("-u", "--upscript", help='Install rpi-update and run it.', action="store_true")
 
 # Save arguments.
 args = parser.parse_args()
@@ -41,11 +44,11 @@ else:
     # https://docs.python.org/3/library/pwd.html
     USERNAMEVAR = pwd.getpwuid(1000)[0]
 # https://docs.python.org/3/library/grp.html
-USERGROUP=grp.getgrgid(pwd.getpwnam(USERNAMEVAR)[3])[0]
-USERHOME=os.path.expanduser("~{0}".format(USERNAMEVAR))
+USERGROUP = grp.getgrgid(pwd.getpwnam(USERNAMEVAR)[3])[0]
+USERHOME = os.path.expanduser("~{0}".format(USERNAMEVAR))
 MACHINEARCH = platform.machine()
-print("Username is:",USERNAMEVAR)
-print("Group Name is:",USERGROUP)
+print("Username is:", USERNAMEVAR)
+print("Group Name is:", USERGROUP)
 
 # Print detected options
 print(args)
@@ -53,9 +56,53 @@ print(args)
 if args.noprompt is False:
     input("Press Enter to continue.")
 
+
 if args.cliutils is True:
     print("Installing command line utilities.")
-    subprocess.run("apt-get update; apt-get install -y avahi-daemon", shell=True)
+    subprocess.run("apt-get update; apt-get install -y avahi-daemon wget curl ca-certificates build-essential", shell=True)
+    groupscript = """
+    # Add normal user to all reasonable groups
+    # Get all groups
+    LISTOFGROUPS="$(cut -d: -f1 /etc/group)"
+    # Remove some groups
+    CUTGROUPS=$(sed -e "/^users/d; /^root/d; /^nobody/d; /^nogroup/d; /^{1}/d" <<< $LISTOFGROUPS)
+    echo Groups to Add: $CUTGROUPS
+    for grp in $CUTGROUPS; do
+        usermod -aG $grp {0}
+    done
+    """.format(USERNAMEVAR, USERGROUP)
+    subprocess.run(groupscript, shell=True)
+    # Disable pulseaudio suspend-on-idle
+    if os.path.isfile("/etc/pulse/system.pa") is True:
+        subprocess.run("sed -i '/load-module module-suspend-on-idle/ s/^#*/#/' /etc/pulse/system.pa", shell=True)
+
+
+if args.upscript is True:
+    print("Installing rpi-update script.")
+    localpiupbinary = "/usr/local/bin/rpi-update"
+    urllib.request.urlretrieve(RPIUPDATEURL, localpiupbinary)
+    os.chmod(localpiupbinary, 0o777)
+    print("Adding and running auto-update for Pi.")
+    aptautoupdate_script = """#!/bin/bash
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+if [ -f {0} ]; then
+    SKIP_WARNING=1 PRUNE_MODULES=1 {0}
+fi
+    """.format(localpiupbinary)
+    cronfolder = "/etc/cron.daily"
+    aptautoupdate_file = cronfolder + "/aptautoupdate"
+    if os.path.isdir(cronfolder) is True:
+        print("Writing {0}".format(aptautoupdate_file))
+        with open(aptautoupdate_file, 'w') as aptautoupdate_file_write:
+            aptautoupdate_file_write.write(aptautoupdate_script)
+        os.chmod(aptautoupdate_file, 0o777)
+        print("Running {0}".format(aptautoupdate_file))
+        subprocess.run(aptautoupdate_file, shell=True)
+    else:
+        print(cronfolder, "does not exist. Please install a cron.")
+
 
 if args.docker is True:
     print("Installing Docker.")
