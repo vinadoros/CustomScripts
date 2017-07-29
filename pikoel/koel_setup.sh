68763796c3683d0f43d3dc2b00274d04d9654b07
Vinadoros <vinadoros@outlook.com>
2017-07-29 12:55:05 -0400

    Fix mariadb setup

--- pikoel/koel_setup.sh
+++ pikoel/koel_setup.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 set -ex
 
-cd /var/www
+cd /var/www/koel
 if [ ! -f ./updated ]; then
-  # Set the root password
-  mysqladmin -u root -h localhost password $DBPASSWD
   # Setup mariadb database.
   if [ ! -z $DBPASSWD ]; then
-    mysql -h mariadb -u root -p$DBPASSWD -e "CREATE DATABASE IF NOT EXISTS koel DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
-    mysql -h mariadb -u root -p$DBPASSWD -e "CREATE USER IF NOT EXISTS 'koel-user';"
-    mysql -h mariadb -u root -p$DBPASSWD -e "SET PASSWORD FOR 'koel-user'@'localhost' = PASSWORD('$DBPASSWD');"
-    mysql -h mariadb -u root -p$DBPASSWD -e "GRANT ALL PRIVILEGES ON koel.* TO 'koel-user'@'localhost' WITH GRANT OPTION;"
+    mysql -u root -p$DBPASSWD -e "CREATE DATABASE IF NOT EXISTS koel DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
+    mysql -u root -p$DBPASSWD -e "CREATE USER IF NOT EXISTS 'koel-user'@'localhost';"
+    mysql -u root -p$DBPASSWD -e "SET PASSWORD FOR 'koel-user'@'localhost'= PASSWORD('$DBPASSWD');"
+    mysql -u root -p$DBPASSWD -e "GRANT ALL PRIVILEGES ON koel.* TO 'koel-user'@'localhost' WITH GRANT OPTION;"
   fi
   # Setup env file
+  if [ ! -f .env ]; then
+    cp .env.example .env
+  fi
   sed -i 's/^ADMIN_EMAIL=.*/ADMIN_EMAIL=admin@example.com/g' .env
   sed -i 's/^ADMIN_NAME=.*/ADMIN_NAME=admin/g' .env
   sed -i 's/^ADMIN_PASSWORD=.*/ADMIN_PASSWORD=pass/g' .env
@@ -21,8 +22,7 @@ if [ ! -f ./updated ]; then
   sed -i 's/^DB_DATABASE=.*/DB_DATABASE=koel/g' .env
   sed -i 's/^DB_USERNAME=.*/DB_USERNAME=koel-user/g' .env
   sed -i "s/^DB_PASSWORD=.*/DB_PASSWORD=$DBPASSWD/g" .env
-  # Update npm and composer packages
-  yarn
+  # Update composer packages
   composer install
   # Init database
   php artisan koel:init
--- pikoel/mariadb_setup.sh
+++ pikoel/mariadb_setup.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
-set -ex
 
 if [ ! -f /var/lib/mysql/ibdata1 ]; then
+  # Create mysql tables
   mysql_install_db
+  # Load mysql in safe mode
+  mysqld_safe &
+  # Wait for the daemon to appear.
+  sleep 3
+  # Set the root password
+  mysqladmin -u root password $DBPASSWD
+  # Hopefully end mysqld.
+  killall mysqld_safe mysqld
+  # Wait until mysqld is gone
+  sleep 3
 else
-  echo "mysql db exists."
+  echo "mariadb files exist."
 fi
+
+# Start mariadb
+/etc/init.d/mysql restart
