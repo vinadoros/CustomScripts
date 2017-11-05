1280f1251f239df093d831e9fa0a3f2c54f72aff
Vinadoros <vinadoros@outlook.com>
2017-11-05 08:17:22 -0500

    Update vagrant config

--- YVagrantConfig.py
+++ YVagrantConfig.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""Create a Vagrant global configuration"""
 
+# Python Includes
 import os
+import multiprocessing
 import shutil
 import subprocess
 import sys
@@ -12,36 +15,35 @@ if os.geteuid() is 0:
     sys.exit("\nError: Please run this script as a normal user (not root).\n")
 
 # Exit if vagrant not installed.
-if shutil.which("vagrant") == None:
+if shutil.which("vagrant") is None:
     sys.exit("\nError: Vagrant not detected. Please install vagrant.\n")
 
 # Get homefolder.
-USERHOME=os.path.expanduser("~")
-print("Home Folder is:",USERHOME)
+USERHOME = os.path.expanduser("~")
+print("Home Folder is:", USERHOME)
 
 # Install vagrant plugins
 # List of vagrant plugins available: https://github.com/mitchellh/vagrant/wiki/Available-Vagrant-Plugins
 subprocess.call("vagrant plugin install vagrant-vbguest vagrant-timezone", shell=True)
 
-# Get CPUs and memory of host
-CPUCOUNT=os.cpu_count()
-if CPUCOUNT >= 4:
-    CPUCOUNT = 4
+# Get CPUs of host
+CPUCOUNT = multiprocessing.cpu_count() if multiprocessing.cpu_count() <= 4 else 4
+# Get memory of host, parse free command, return only physical free memory (the first column and first row).
+MEMORY = int(os.popen('free -t -m').readlines()[-3].split()[1:][0])
 # Install vagrant user configuration
-VAGRANTCONFIG="""
+VAGRANTCONFIG = """
 Vagrant.configure("2") do |config|
   if Vagrant.has_plugin?("vagrant-timezone")
     config.timezone.value = :host
   end
   config.vm.provider "virtualbox" do |v|
-    v.memory = 2048
+    v.memory = {1}
     v.cpus = {0}
   end
 end
-""".format(CPUCOUNT)
-VAGRANTFOLDER=USERHOME+"/.vagrant.d/"
-if os.path.isdir(VAGRANTFOLDER) == True:
+""".format(CPUCOUNT, MEMORY/8)
+VAGRANTFOLDER = USERHOME+"/.vagrant.d/"
+if os.path.isdir(VAGRANTFOLDER) is True:
     print("Writing {0}".format(VAGRANTFOLDER+"Vagrantfile"))
-    f = open(VAGRANTFOLDER+"Vagrantfile","w")
-    f.write(VAGRANTCONFIG)
-    f.close()
+    with open(VAGRANTFOLDER+"Vagrantfile", "w") as f:
+        f.write(VAGRANTCONFIG)
