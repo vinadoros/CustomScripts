bd6f737abd141fcbda4127b7f192c0594c08dc0b
Vinadoros <vinadoros@outlook.com>
2017-11-17 22:15:11 -0500

    Some initial koel fixes (still broken)

--- docker_koel/startup.sh
+++ docker_koel/startup.sh
@@ -9,24 +9,22 @@ cd /var/www/koel
 # Setup env file
 if [ ! -f .env ]; then
   cp .env.example .env
-fi
-# Prepare database
-if [ -f /db.sqlite ]; then
-	DBEMPTY=0
+  # Prepare database
+  mysql -h db -u root -p$DBPASSWD -e "CREATE DATABASE IF NOT EXISTS ampache;"
+  DBEMPTY=1
 else
-	DBEMPTY=1
-	touch /db.sqlite
+  DBEMPTY=0
 fi
-chown www-data:www-data /db.sqlite
-chmod a+rw /db.sqlite
+chown www-data:www-data /var/www/db.sqlite
+chmod a+rw /var/www/db.sqlite
 sed -i 's/^ADMIN_EMAIL=.*/ADMIN_EMAIL=a@l.c/g' .env
 sed -i 's/^ADMIN_NAME=.*/ADMIN_NAME=admin/g' .env
 sed -i "s/^ADMIN_PASSWORD=.*/ADMIN_PASSWORD=$ADMINPASS/g" .env
-sed -i 's/^DB_CONNECTION=.*/DB_CONNECTION=sqlite/g' .env
-#sed -i 's/^DB_HOST=.*/DB_HOST=localhost/g' .env
-sed -i 's/^DB_DATABASE=.*/DB_DATABASE=\/db.sqlite/g' .env
-#sed -i 's/^DB_USERNAME=.*/DB_USERNAME=koel-user/g' .env
-#sed -i "s/^DB_PASSWORD=.*/DB_PASSWORD=$DBPASSWD/g" .env
+sed -i 's/^DB_CONNECTION=.*/DB_CONNECTION=mysql/g' .env
+sed -i 's/^DB_HOST=.*/DB_HOST=db/g' .env
+sed -i 's/^DB_DATABASE=.*/DB_DATABASE=koel/g' .env
+sed -i "s/^DB_USERNAME=.*/DB_USERNAME=root/g" .env
+sed -i "s/^DB_PASSWORD=.*/DB_PASSWORD=$DBPASSWD/g" .env
 if [ $DBEMPTY = 1 ]; then
   # Update composer packages
   composer install
@@ -40,7 +38,7 @@ fi
 # Start php-fpm
 /etc/init.d/php7.0-fpm start
 # Set permissions on folder and start nginx
-chown www-data:www-data -R /var/www/koel
+chown www-data:www-data -R /var/www/
 /etc/init.d/nginx start
 # To run nginx in foreground as root
 # nginx -g 'daemon off;'
