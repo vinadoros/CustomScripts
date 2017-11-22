7ede4a361f1957d986b1225efc97a3d0a33c81b4
Vinadoros <vinadoros@outlook.com>
2017-11-22 08:13:31 -0500

    Add force flag to refresh portainer.

--- CDocker.py
+++ CDocker.py
@@ -19,6 +19,7 @@ SCRIPTDIR = sys.path[0]
 parser = argparse.ArgumentParser(description='Install Docker.')
 parser.add_argument("-n", "--noprompt", help='Do not prompt to continue.', action="store_true")
 parser.add_argument("-r", "--release", help='Force operating system release. Set this if a particular release should be forced.')
+parser.add_argument("-f", "--force", help='Force re-installation of portainer.', action="store_true")
 
 # Save arguments.
 args = parser.parse_args()
@@ -74,10 +75,18 @@ dnf config-manager --set-enabled docker-ce-edge
 
 ### Docker Configuration ###
 if shutil.which("docker"):
+    # Command to install portainer.
+    portainer_name = "portainer"
+    portainer_cmd = "docker pull portainer/portainer; docker run -d --name {0} --restart=always -p 61234:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer".format(portainer_name)
     # Check for portainer.
-    if "portainer" not in CFunc.subpout("docker ps -a"):
-        # Install portainer.
-        subprocess.run("docker run -d --name portainer --restart=always -p 61234:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer", shell=True)
+    portainer_check = CFunc.subpout("docker ps -a")
+    # Remove portainer if force was set and portainer was detected.
+    if "portainer" in portainer_check and args.force is True:
+        subprocess.run("docker stop {0}; docker rm {0}".format(portainer_name), shell=True)
+    # Install portainer if not found.
+    if "portainer" not in portainer_check or args.force is True:
+        print("Installing portainer.")
+        subprocess.run(portainer_cmd, shell=True)
     else:
         print("\nPortainer detected. Not re-installing.")
 else:
