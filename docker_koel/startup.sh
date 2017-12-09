cbbc5ba6d035ec88aa0d230d4a66d8d20b935bc7
Vinadoros <vinadoros@outlook.com>
2017-12-08 20:56:06 -0500

    More koel changes

--- docker_koel/startup.sh
+++ docker_koel/startup.sh
@@ -4,19 +4,20 @@
 # https://koel.phanan.net/docs
 if [ ! -d /var/www/koel ]; then
   git clone https://github.com/phanan/koel.git /var/www/koel
+  REPOEMPTY=1
+else
+  REPOEMPTY=0
 fi
 cd /var/www/koel
 # Setup env file
 if [ ! -f .env ]; then
   cp .env.example .env
   # Prepare database
-  mysql -h db -u root -p$DBPASSWD -e "CREATE DATABASE IF NOT EXISTS ampache;"
+  mysql -h db -u root -p$DBPASSWD -e "CREATE DATABASE IF NOT EXISTS koel;"
   DBEMPTY=1
 else
   DBEMPTY=0
 fi
-chown www-data:www-data /var/www/db.sqlite
-chmod a+rw /var/www/db.sqlite
 sed -i "s/^ADMIN_EMAIL=.*/ADMIN_EMAIL=$ADMINUSER/g" .env
 sed -i 's/^ADMIN_NAME=.*/ADMIN_NAME=admin/g' .env
 sed -i "s/^ADMIN_PASSWORD=.*/ADMIN_PASSWORD=$ADMINPASS/g" .env
@@ -25,12 +26,16 @@ sed -i 's/^DB_HOST=.*/DB_HOST=db/g' .env
 sed -i 's/^DB_DATABASE=.*/DB_DATABASE=koel/g' .env
 sed -i "s/^DB_USERNAME=.*/DB_USERNAME=root/g" .env
 sed -i "s/^DB_PASSWORD=.*/DB_PASSWORD=$DBPASSWD/g" .env
-if [ $DBEMPTY = 1 ]; then
-  # Update composer packages
+if [ $REPOEMPTY = 1 ]; then
+  # Install composer packages
   composer install
-  composer update
-  # Init database
-  php artisan koel:init
+fi
+
+# Ensure everything is up to date
+composer update
+php artisan koel:init
+if [ $REPOEMPTY = 0 ]; then
+  php artisan koel:sync
 fi
 
 # Start php-fpm
