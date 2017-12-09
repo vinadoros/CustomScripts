f5c2e8e99cca3baf0c3a8c73042d0728c1d92745
Vinadoros <vinadoros@outlook.com>
2017-12-09 12:56:10 -0500

    Revert ubuntu vbox guest logic

--- MUbuntu.py
+++ MUbuntu.py
@@ -264,7 +264,11 @@ if not args.nogui or not args.bare:
 if vmstatus == "kvm":
     CFunc.aptinstall("spice-vdagent qemu-guest-agent")
 if vmstatus == "vbox":
-    subprocess.run("{0}/CVBoxGuest.py -n".format(SCRIPTDIR), shell=True)
+    CFunc.aptinstall("virtualbox-guest-utils virtualbox-guest-dkms dkms")
+    if not args.nogui:
+        CFunc.aptinstall("virtualbox-guest-x11")
+    subprocess.run("gpasswd -a {0} vboxsf".format(USERNAMEVAR), shell=True)
+    subprocess.run("systemctl enable virtualbox-guest-utils", shell=True)
 if vmstatus == "vmware":
     CFunc.aptinstall("open-vm-tools open-vm-tools-dkms")
     if not args.nogui:
