c7fc840906727bde7c41d29ff60beb741aff980b
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2017-12-19 11:56:31 -0500

    Add retries to tablacus

--- Win_tablacus.ps1
+++ Win_tablacus.ps1
@@ -1,19 +1,32 @@
 
-# Install/upgrade Tablacus
-choco upgrade -y tablacus
+# https://stackoverflow.com/questions/41554300/how-to-run-retry-the-commands-multiple-times-in-try-block
+$success = $false
+$attempt = 10
+while ($attempt -gt 0 -and -not $success){
+  try{
 
-# Tablacus configuration
-$pathtotablacus = "$env:PROGRAMDATA\chocolatey\lib\tablacus\tools\"
-if(Test-Path -Path $pathtotablacus){
-  # Set rwx everyone permissions for tablacus folder
-  $Acl = Get-ACL $pathtotablacus
-  $AccessRule= New-Object System.Security.AccessControl.FileSystemAccessRule("everyone","full","ContainerInherit,Objectinherit","none","Allow")
-  $Acl.AddAccessRule($AccessRule)
-  Set-Acl $pathtotablacus $Acl
-  # Create shortcut for tablacus
-  $WshShell = New-Object -comObject WScript.Shell
-  $Shortcut = $WshShell.CreateShortcut("$env:PUBLIC\Desktop\Tablacus.lnk")
-  $Shortcut.TargetPath = "$pathtotablacus\TE64.exe"
-  $Shortcut.WorkingDirectory = "$pathtotablacus"
-  $Shortcut.Save()
+    # Install/upgrade Tablacus
+    choco upgrade -y tablacus
+
+    # Tablacus configuration
+    $pathtotablacus = "$env:PROGRAMDATA\chocolatey\lib\tablacus\tools\"
+    if(Test-Path -Path $pathtotablacus){
+      # Set rwx everyone permissions for tablacus folder
+      $Acl = Get-ACL $pathtotablacus
+      $AccessRule= New-Object System.Security.AccessControl.FileSystemAccessRule("everyone","full","ContainerInherit,Objectinherit","none","Allow")
+      $Acl.AddAccessRule($AccessRule)
+      Set-Acl $pathtotablacus $Acl
+      # Create shortcut for tablacus
+      $WshShell = New-Object -comObject WScript.Shell
+      $Shortcut = $WshShell.CreateShortcut("$env:PUBLIC\Desktop\Tablacus.lnk")
+      $Shortcut.TargetPath = "$pathtotablacus\TE64.exe"
+      $Shortcut.WorkingDirectory = "$pathtotablacus"
+      $Shortcut.Save()
+    }
+
+    $success = $true
+  } catch {
+      echo "ERROR: Attempt $attempt failed."
+      $attempt--
+  }
 }
