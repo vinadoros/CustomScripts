54ad276d134a7f9ad4918f63c5e93e7b300461e5
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2018-02-14 19:44:52 -0500

    Add kde-runtime

--- MUbuntu.py
+++ MUbuntu.py
@@ -248,6 +248,7 @@ elif args.desktop == "kde":
         CFunc.aptdistupg()
         CFunc.aptinstall("neon-desktop")
         CFunc.aptdistupg()
+        CFunc.aptinstall("kde-runtime")
     else:
         print("\n Installing kde desktop")
         CFunc.aptinstall("kubuntu-desktop")
