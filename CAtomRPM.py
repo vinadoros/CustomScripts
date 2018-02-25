bf0baf0f1266be64e3c20575c07fe3220a6a781b
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2018-02-25 10:39:36 -0500

    Add cron to AtomRPM

--- CAtomRPM.py
+++ CAtomRPM.py
@@ -15,23 +15,21 @@ if os.geteuid() is not 0:
 
 MACHINEARCH = platform.machine()
 
-# Atom variables
-ATOMRPMFILE = "/tmp/atom.x86_64.rpm"
-ATOMRPMURL = "https://atom.io/download/rpm"
+if MACHINEARCH == "x86_64":
+    # Cron update script (Fedora specifc)
+    CRONSCRIPT = """#!/bin/bash
+ATOM_INSTALLED_VERSION=$(rpm -qi atom | grep "Version" |  cut -d ':' -f 2 | cut -d ' ' -f 2)
+ATOM_LATEST_VERSION=$(curl -sL "https://api.github.com/repos/atom/atom/releases/latest" | grep -E "https.*atom-amd64.tar.gz" | cut -d '"' -f 4 | cut -d '/' -f 8 | sed 's/v//g')
 
-# Find version on GitHub
-github_link = subprocess.run("""curl -I https://github.com/atom/atom/releases/latest | perl -n -e '/^Location: (.*)$/ && print "$1"'""", shell=True, stdout=subprocess.PIPE, universal_newlines=True)
-version_available = subprocess.run("""basename {0} | perl -n -e '/(\d+.\d+.\d+)/ && print "$1"'""".format(github_link.stdout.strip()), shell=True, stdout=subprocess.PIPE, universal_newlines=True)
-version_available = version_available.stdout.strip()
-print("Version available:", version_available)
-version_installed = subprocess.run("""rpm -q atom | perl -n -e '/atom-(.*?)-/ && print "$1"'""", shell=True, stdout=subprocess.PIPE, universal_newlines=True)
-version_installed = version_installed.stdout.strip()
-print("Version installed:", version_installed)
-if version_available != version_installed and MACHINEARCH == "x86_64":
-    print("Downloading", ATOMRPMURL, "to", ATOMRPMFILE)
-    urllib.request.urlretrieve(ATOMRPMURL, ATOMRPMFILE)
-    # Install it.
-    subprocess.run("rpm -ivh {0}".format(ATOMRPMFILE), shell=True)
-    os.remove(ATOMRPMFILE)
+if [[ $ATOM_INSTALLED_VERSION < $ATOM_LATEST_VERSION ]]; then
+  sudo dnf install -y https://github.com/atom/atom/releases/download/v${ATOM_LATEST_VERSION}/atom.x86_64.rpm
+fi
+"""
+    with open('/etc/cron.daily/atomupdate', 'w') as file:
+        file.write(CRONSCRIPT)
+    os.chmod('/etc/cron.daily/atomupdate', 0o777)
+
+    # Run the cron script to install atom.
+    subprocess.run("/etc/cron.daily/atomupdate", shell=True)
 else:
-    print("Versions are equal, skipping install.")
+    print("Not x86_64, skipping install.")
