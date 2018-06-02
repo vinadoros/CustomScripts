ad8f39d3b53344a0683a30654be0e68e88e0fd52
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2018-06-02 10:15:08 -0400

    Add dns for Win7

--- unattend/win_initial.bat
+++ unattend/win_initial.bat
@@ -6,6 +6,8 @@ C:\Windows\SysWOW64\cmd.exe /c powershell -Command "Set-ExecutionPolicy -Executi
 
 REM Add IPv4 dns for virtual machines
 netsh interface ipv4 add dnsserver "Ethernet" address=1.1.1.1 index=1
+REM Same for Windows 7
+netsh interface ipv4 add dnsserver "Local Area Network" address=1.1.1.1 index=1
 
 REM Install Chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
