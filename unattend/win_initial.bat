45fcab75ab44fd6e380395600dc54419712259af
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2018-06-19 07:51:27 -0400

    Change to 9s for DNS

--- unattend/win_initial.bat
+++ unattend/win_initial.bat
@@ -5,9 +5,9 @@ echo Set Execution Policy 32 Bit
 C:\Windows\SysWOW64\cmd.exe /c powershell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
 
 REM Add IPv4 dns for virtual machines
-netsh interface ipv4 add dnsserver "Ethernet" address=1.1.1.1 index=1
+netsh interface ipv4 add dnsserver "Ethernet" address=9.9.9.9 index=1
 REM Same for Windows 7
-netsh interface ipv4 add dnsserver "Local Area Network" address=1.1.1.1 index=1
+netsh interface ipv4 add dnsserver "Local Area Network" address=9.9.9.9 index=1
 
 REM Install Chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
