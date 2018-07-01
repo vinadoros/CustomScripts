eee094fd94dbca7af704231abacbc1020008a7ea
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2018-07-01 14:48:25 -0400

    Remove ipv4 dns for windows

--- unattend/win_initial.bat
+++ unattend/win_initial.bat
@@ -4,11 +4,6 @@ cmd.exe /c powershell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigne
 echo Set Execution Policy 32 Bit
 C:\Windows\SysWOW64\cmd.exe /c powershell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
 
-REM Add IPv4 dns for virtual machines
-netsh interface ipv4 add dnsserver "Ethernet" address=9.9.9.9 index=1
-REM Same for Windows 7
-netsh interface ipv4 add dnsserver "Local Area Network" address=9.9.9.9 index=1
-
 REM Install Chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 "%ALLUSERSPROFILE%\chocolatey\bin\choco.exe" upgrade -y dotnetfx powershell
