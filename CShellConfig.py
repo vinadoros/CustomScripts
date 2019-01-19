62660ae8d68b1f8ee3374d8ca85485eb6ed64837
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2019-01-18 22:53:47 -0500

    Re-add fish config

--- CShellConfig.py
+++ CShellConfig.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-"""Configure enhancements for shell."""
+"""Configure enhancements for shells."""
 
 # Python includes.
+import argparse
 import os
 import sys
 import subprocess
 import shutil
+import tempfile
 # Custom includes
 import CFunc
 
@@ -14,23 +16,23 @@ print("Running {0}".format(__file__))
 # Folder of this script
 SCRIPTDIR = sys.path[0]
 
+# Get arguments
+parser = argparse.ArgumentParser(description='Configure enhancements for shells.')
+parser.add_argument("-z", "--zsh", help='Configure zsh.', action="store_true")
+parser.add_argument("-f", "--fish", help='Configure fish.', action="store_true")
+args = parser.parse_args()
+
+# Check if we are root.
+rootstate = CFunc.is_root(checkstate=True, state_exit=False)
 # Get non-root user information.
-USERNAMEVAR, USERGROUP, USERHOME = CFunc.getnormaluser()
+USERNAMEVAR, USERGROUP, USERVARHOME = CFunc.getnormaluser()
 # Note: This folder is the root home folder.
 ROOTHOME = os.path.expanduser("~root")
-# This folder is the above detected user's home folder if this script is run as root.
-USERVARHOME = os.path.expanduser("~{0}".format(USERNAMEVAR))
 
 # Detect OS information
 distro, release = CFunc.detectdistro()
 print("Distro is {0}.".format(distro))
 print("Release is {0}.".format(release))
-
-# Ensure that certain commands exist.
-cmdcheck = ["chsh"]
-for cmd in cmdcheck:
-    if not shutil.which(cmd):
-        sys.exit("\nError, ensure command {0} is installed.".format(cmd))
 
 
 ### Generic Section ###
@@ -308,27 +310,27 @@ BASHSCRIPT += rc_additions
 # Set bash script
 BASHSCRIPTPATH = os.path.join(USERVARHOME, ".bashrc")
 print("Bash script path is {0}".format(BASHSCRIPTPATH))
-if os.geteuid() is 0:
+if rootstate is True:
     BASHROOTSCRIPTPATH = os.path.join(ROOTHOME, ".bashrc")
     print("Bash root script path is {0}".format(BASHROOTSCRIPTPATH))
 
 # Remove existing bash scripts and copy skeleton.
 if os.path.isfile(BASHSCRIPTPATH):
     os.remove(BASHSCRIPTPATH)
-if os.geteuid() is 0:
+if rootstate is True:
     if os.path.isfile(BASHROOTSCRIPTPATH):
         os.remove(BASHROOTSCRIPTPATH)
 
 # Skeleton will get overwritten by bash-it below, this is left here just in case it is needed in the future.
 if os.path.isfile("/etc/skel/.bashrc"):
     shutil.copy("/etc/skel/.bashrc", BASHSCRIPTPATH)
-    if os.geteuid() is 0:
+    if rootstate is True:
         shutil.copy("/etc/skel/.bashrc", BASHROOTSCRIPTPATH)
         shutil.chown(BASHSCRIPTPATH, USERNAMEVAR, USERGROUP)
 else:
     # Create bashrc if no skeleton
     open(BASHSCRIPTPATH, 'a').close()
-    if os.geteuid() is 0:
+    if rootstate is True:
         open(BASHROOTSCRIPTPATH, 'a').close()
         shutil.chown(BASHSCRIPTPATH, USERNAMEVAR, USERGROUP)
 
@@ -343,7 +345,7 @@ if os.path.isdir("/opt/bash-it"):
     /opt/bash-it/install.sh --silent
     """.format(ROOTHOME), shell=True)
     subprocess.run("""sed -i -- "s/BASH_IT_THEME=.*/BASH_IT_THEME='powerline'/g" {0}""".format(BASHSCRIPTPATH), shell=True)
-    if os.geteuid() is 0:
+    if rootstate is True:
         CFunc.run_as_user(USERNAMEVAR, "/opt/bash-it/install.sh --silent", shutil.which("bash"))
         subprocess.run("""sed -i -- "s/BASH_IT_THEME=.*/BASH_IT_THEME='powerline'/g" {0} {1}""".format(BASHROOTSCRIPTPATH, BASHSCRIPTPATH), shell=True)
 
@@ -352,7 +354,7 @@ BASHSCRIPT_VAR = open(BASHSCRIPTPATH, mode='a')
 BASHSCRIPT_VAR.write(BASHSCRIPT)
 BASHSCRIPT_VAR.close()
 os.chmod(BASHSCRIPTPATH, 0o644)
-if os.geteuid() is 0:
+if rootstate is True:
     BASHSCRIPTUSER_VAR = open(BASHROOTSCRIPTPATH, mode='a')
     BASHSCRIPTUSER_VAR.write(BASHSCRIPT)
     BASHSCRIPTUSER_VAR.close()
@@ -362,22 +364,18 @@ if os.geteuid() is 0:
 # Modify system path for debian
 # https://serverfault.com/questions/166383/how-set-path-for-all-users-in-debian
 logindefs_file = os.path.join("/", "etc", "login.defs")
-if os.geteuid() is 0 and distro == "Debian" and os.path.isfile(logindefs_file):
+if rootstate is True and distro == "Debian" and os.path.isfile(logindefs_file):
     print("Modifying {0}".format(logindefs_file))
     if CFunc.find_pattern_infile(logindefs_file, "ENV_PATH.*PATH.*{0}".format(os.path.basename(SCRIPTDIR))) is False:
         subprocess.run("""sed -i '/^ENV_PATH.*PATH.*/ s@$@:{1}@' {0}""".format(logindefs_file, SCRIPTDIR), shell=True)
     if CFunc.find_pattern_infile(logindefs_file, "ENV_SUPATH.*PATH.*{0}".format(os.path.basename(SCRIPTDIR))) is False:
         subprocess.run("""sed -i '/^ENV_SUPATH.*PATH.*/ s@$@:{1}@' {0}""".format(logindefs_file, SCRIPTDIR), shell=True)
 
+
 ######### Zsh Section #########
 # Check if zsh exists
-if shutil.which('zsh'):
+if args.zsh is True and shutil.which('zsh'):
     ZSHPATH = shutil.which('zsh')
-    # Change the shell for the non-root user if running as root.
-    if os.geteuid() is 0:
-        subprocess.run('chsh -s {ZSHPATH} {USERNAMEVAR}'.format(USERNAMEVAR=USERNAMEVAR, ZSHPATH=ZSHPATH), shell=True)
-    else:
-        subprocess.run('chsh -s {0}'.format(ZSHPATH), shell=True)
 
     # Install oh-my-zsh for user
     CFunc.gitclone("git://github.com/robbyrussell/oh-my-zsh.git", "{0}/.oh-my-zsh".format(USERVARHOME))
@@ -417,8 +415,312 @@ fi
 else:
     print("zsh not detected, skipping configuration.")
 
+
+######### Fish Section #########
+# Check if fish exists
+if args.fish is True and shutil.which('fish'):
+
+    # Test for omf
+    if rootstate is True:
+        status = CFunc.run_as_user(USERNAMEVAR, "omf update", shutil.which("fish"))
+    else:
+        status = subprocess.Popen("omf update", shell=True, executable=shutil.which("fish")).returncode
+
+    # Install omf and plugins, since status was not 0
+    if status != 0:
+        # Install omf
+        omf_git_path = os.path.join(tempfile.gettempdir(), "oh-my-fish")
+        CFunc.gitclone("https://github.com/oh-my-fish/oh-my-fish", omf_git_path)
+        if rootstate is True:
+            CFunc.run_as_user(USERNAMEVAR, "cd {0}; bin/install --offline --noninteractive".format(omf_git_path), shutil.which("fish"))
+        else:
+            subprocess.Popen("cd {0}; bin/install --offline --noninteractive", shell=True, executable=shutil.which("fish"))
+        shutil.rmtree(omf_git_path)
+        # Install bobthefish
+        if rootstate is True:
+            CFunc.run_as_user(USERNAMEVAR, "omf install bobthefish", shutil.which("fish"))
+        else:
+            subprocess.Popen("omf install bobthefish", shell=True, executable=shutil.which("fish"))
+
+    # Personal note: To uninstall omf completely, use the following command as a normal user:
+    # omf destroy; sudo rm -rf /root/.config/omf/ /root/.cache/omf/ /root/.local/share/omf/ ~/.config/omf/ ~/.cache/omf/ ~/.local/share/omf/
+
+    # Generate fish script.
+    FISHSCRIPT = """
+# Set bobthefish options
+set -g theme_display_user yes
+# Set root and non-root cmds.
+if [ (id -u) != "0" ]
+    set SUDOCMD "sudo"
+else
+    set SUDOCMD ""
+end
+set CUSTOMSCRIPTPATH "{SCRIPTDIR}"
+# Set editor
+set -gx EDITOR nano
+set -gx XZ_OPT "-T0"
+# Set sbin in path
+if not echo $PATH | grep -q "/sbin"
+    set -gx PATH $PATH /usr/local/sbin /usr/sbin /sbin
+end
+# Set Custom Scripts in path
+if test -d "$CUSTOMSCRIPTPATH"
+    set -gx PATH $PATH "$CUSTOMSCRIPTPATH"
+end
+function sl
+    xhost +localhost >> /dev/null
+    env DISPLAY=$DISPLAY sudo bash
+end
+if [ (id -u) != "0" ]
+    function pc
+        set -x EXISTPATH (pwd)
+        cd "$CUSTOMSCRIPTPATH"
+        git fetch --all
+        git diff
+        git status
+        if not test -z $argv
+            git add -A
+            git commit -m "$argv"
+            git pull
+            git push
+        else
+            echo "No commit message entered. Exiting."
+        end
+        git pull
+        cd "$EXISTPATH"
+        set -e EXISTPATH
+    end
+end
+function sst
+    ssh -t $argv "tmux attach; or tmux new"
+end
+function start
+    echo "Starting systemd service $argv."
+    sudo systemctl start $argv
+    sudo systemctl status -l $argv
+end
+function stop
+    echo "Stopping systemd service $argv."
+    sudo systemctl stop $argv
+    sudo systemctl status -l $argv
+end
+function en
+    echo "Enabling systemd service $argv."
+    sudo systemctl enable $argv
+    sudo systemctl status -l $argv
+end
+function dis
+    echo "Disabling systemd service $argv."
+    sudo systemctl disable $argv
+    sudo systemctl status -l $argv
+end
+function res
+    echo "Restarting systemd service $argv."
+    sudo systemctl restart $argv
+    sudo systemctl status -l $argv
+end
+function st
+    echo "Getting status for systemd service $argv."
+    sudo systemctl status -l $argv
+end
+function dr
+    echo "Executing systemd daemon-reload."
+    sudo systemctl daemon-reload
+end
+function startu
+    echo "Starting systemd service $argv for user."
+    systemctl --user start $argv
+    systemctl --user status -l $argv
+end
+function stopu
+    echo "Stopping systemd service $argv for user."
+    systemctl --user stop $argv
+    systemctl --user status -l $argv
+end
+function resu
+    echo "Restarting systemd service $argv for user."
+    systemctl --user restart $argv
+    systemctl --user status -l $argv
+end
+function stu
+    echo "Getting status for systemd service $argv for user."
+    systemctl --user status -l $argv
+end
+function dru
+    echo "Executing systemd daemon-reload for user."
+    systemctl --user daemon-reload
+end
+# Set package manager functions
+if type -q zypper
+    function ins
+        echo "Installing $argv."
+        sudo zypper install --no-recommends $argv
+    end
+    function inr
+        echo "Installing $argv with recommends."
+        sudo zypper install $argv
+    end
+    function iny
+        echo "Installing $argv."
+        sudo zypper install -yl $argv
+    end
+    function rmv
+        echo "Removing $argv."
+        sudo zypper remove -u $argv
+    end
+    function se
+        echo "Searching for $argv."
+        sudo zypper search $argv
+        sudo zypper info $argv
+    end
+    function cln
+        echo "No clean yet."
+    end
+    function up
+        echo "Updating system."
+        sudo zypper up -yl --no-recommends
+    end
+    function dup
+        echo "Dist-upgrading system."
+        sudo zypper dup --no-recommends
+    end
+else if type -q apt-get
+    function ins
+        echo "Installing $argv."
+        sudo apt-get install $argv
+    end
+    function iny
+        echo "Installing $argv."
+        sudo apt-get install -y $argv
+    end
+    function afix
+        echo "Running apt-get -f install."
+        sudo apt-get -f install
+    end
+    function rmv
+        echo "Removing $argv."
+        sudo apt-get --purge remove $argv
+    end
+    function agu
+        echo "Updating Repos."
+        sudo apt-get update
+    end
+    function se
+        echo "Searching for $argv."
+        apt-cache search $argv
+        echo "Policy for $argv."
+        apt-cache policy $argv
+    end
+    function cln
+        echo "Auto-cleaning cache."
+        sudo apt-get autoclean
+        echo "Auto-removing packages."
+        sudo apt-get autoremove --purge
+    end
+    function up
+        echo "Updating and Dist-upgrading system."
+        sudo apt-get update
+        sudo apt-get dist-upgrade
+    end
+else if type -q dnf; or type -q yum
+    if type -q dnf
+        set PKGMGR dnf
+    else if type -q yum
+        set PKGMGR yum
+    end
+    function ins
+        echo "Installing $argv."
+        sudo $PKGMGR install $argv
+    end
+    function iny
+        echo "Installing $argv."
+        sudo $PKGMGR install -y $argv
+    end
+    function rmv
+        echo "Removing $argv."
+        sudo $PKGMGR remove $argv
+    end
+    function se
+        echo -e "\\nSearching for $argv."
+        sudo $PKGMGR search $argv
+        echo -e "\\nSearching installed packages for $argv."
+        sudo $PKGMGR list installed | grep -i $argv
+        echo -e "\\nInfo for $argv."
+        sudo $PKGMGR info $argv
+    end
+    function cln
+        echo "Auto-removing packages."
+        sudo $PKGMGR autoremove
+    end
+    function up
+        echo "Updating system."
+        sudo $PKGMGR update -y
+    end
+end
+""".format(SCRIPTDIR=SCRIPTDIR)
+
+    # Set fish script
+    FISHSCRIPTUSERPATH = os.path.join(USERVARHOME, ".config", "fish", "config.fish")
+    # Create path if it doesn't existing
+    os.makedirs(os.path.dirname(FISHSCRIPTUSERPATH), exist_ok=True)
+
+    # Install fish script for user (overwrite previous script)
+    with open(FISHSCRIPTUSERPATH, mode='w') as f:
+        f.write(FISHSCRIPT)
+    os.chmod(FISHSCRIPTUSERPATH, 0o644)
+
+    if rootstate is True:
+        subprocess.run("chown -R {0}:{1} {2}".format(USERNAMEVAR, USERGROUP, os.path.dirname(FISHSCRIPTUSERPATH)), shell=True)
+
+
+
 # Fix permissions of home folder if the script was run as root.
-if os.geteuid() is 0:
+if rootstate is True:
     subprocess.run("chown -R {0}:{1} {2}".format(USERNAMEVAR, USERGROUP, USERVARHOME), shell=True)
+
+
+# Change the default shell for GUI terminals
+default_shell_value = shutil.which("sh")
+if shutil.which("fish") and args.fish is True:
+    default_shell_value = shutil.which("fish")
+elif shutil.which("zsh") and args.zsh is True:
+    default_shell_value = shutil.which("zsh")
+elif shutil.which("bash"):
+    default_shell_value = shutil.which("bash")
+
+# Generate script to run to change the default shells.
+DEFAULT_SHELL_SCRIPT = ""
+# Gnome Terminal
+if shutil.which("gnome-terminal") and shutil.which("dconf"):
+    DEFAULT_SHELL_SCRIPT += """
+dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/use-custom-command true
+dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/custom-command "'{0}'"
+""".format(default_shell_value)
+# Tilix
+if shutil.which("tilix") and shutil.which("dconf"):
+    DEFAULT_SHELL_SCRIPT += """
+dconf write /com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/use-custom-command true
+dconf write /com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/custom-command "'{0}'"
+""".format(default_shell_value)
+# MATE Terminal
+if shutil.which("mate-terminal") and shutil.which("dconf"):
+    DEFAULT_SHELL_SCRIPT += """
+dconf write /org/mate/terminal/profiles/default/use-custom-command true
+dconf write /org/mate/terminal/profiles/default/custom-command "'{0}'"
+""".format(default_shell_value)
+if shutil.which("konsole") and shutil.which("kwriteconfig5"):
+    DEFAULT_SHELL_SCRIPT += """
+kwriteconfig5 --file konsolerc --group "Desktop Entry" --key DefaultProfile "default.profile"
+mkdir -p ~/.local/share/konsole
+kwriteconfig5 --file ~/.local/share/konsole/default.profile --group "General" --key Command "{0}"
+""".format(default_shell_value)
+
+# Run the script
+if rootstate is True:
+    # Change shells for user.
+    CFunc.run_as_user(USERNAMEVAR, DEFAULT_SHELL_SCRIPT)
+else:
+    # Change the shells for this user.
+    subprocess.run(DEFAULT_SHELL_SCRIPT, shell=True)
 
 print("Script finished successfully.")
