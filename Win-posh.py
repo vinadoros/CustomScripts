883bc873feab60fa81884e994b8f30da9698c75e
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2019-06-08 08:28:37 -0400

    Powershell profile updates

--- Win-posh.py
+++ Win-posh.py
@@ -40,33 +40,13 @@ Install-Module -Name 'Get-ChildItemColor'
 # Install powershell profile
 powershell_profile_folder = os.path.join(USERHOME, "Documents", "WindowsPowerShell")
 powershell_profile_script = os.path.join(powershell_profile_folder, "Microsoft.PowerShell_profile.ps1")
-powershell_profile_text = """# Ensure that Get-ChildItemColor is loaded
+powershell_profile_text = """<#
+.SYNOPSIS
+  Powershell Profile.
+#>
+ 
+# Ensure that Get-ChildItemColor is loaded
 Import-Module Get-ChildItemColor
-
-# Set l and ls alias to use the new Get-ChildItemColor cmdlets
-Set-Alias la Get-ChildItemColor -Option AllScope
-Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
-
-# Helper function to show Unicode character
-function U
-{
-    param
-    (
-        [int] $Code
-    )
-
-    if ((0 -le $Code) -and ($Code -le 0xFFFF))
-    {
-        return [char] $Code
-    }
-
-    if ((0x10000 -le $Code) -and ($Code -le 0x10FFFF))
-    {
-        return [char]::ConvertFromUtf32($Code)
-    }
-
-    throw "Invalid character code $Code"
-}
 
 # Ensure posh-git is loaded
 Import-Module -Name posh-git
@@ -76,6 +56,30 @@ Import-Module -Name oh-my-posh
 
 # Default the prompt to agnoster oh-my-posh theme
 Set-Theme agnoster
+
+
+### Functions ###
+Function Format-FileSize() {
+    Param ([int]$size)
+    If     ($size -gt 1TB) {[string]::Format("{0:0.00} TB", $size / 1TB)}
+    ElseIf ($size -gt 1GB) {[string]::Format("{0:0.00} GB", $size / 1GB)}
+    ElseIf ($size -gt 1MB) {[string]::Format("{0:0.00} MB", $size / 1MB)}
+    ElseIf ($size -gt 1KB) {[string]::Format("{0:0.00} kB", $size / 1KB)}
+    ElseIf ($size -gt 0)   {[string]::Format("{0:0.00} B", $size)}
+    Else                   {""}
+}
+Function Fcn-List-All {
+    Get-ChildItemColor | Select-Object Mode, @{Name="Size";Expression={Format-FileSize($_.Length)}}, LastWriteTime, Name
+}
+
+
+### Aliases ###
+# Set la and ls alias to use the new Get-ChildItemColor cmdlets
+# Set-Alias -Name la -Value Fcn-List-All
+Set-Alias l Get-ChildItemColor -option AllScope
+Set-Alias la Get-ChildItemColor -option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
+
 """
 os.makedirs(powershell_profile_folder, exist_ok=True)
 with open(powershell_profile_script, 'w') as powershell_profile_script_handle:
