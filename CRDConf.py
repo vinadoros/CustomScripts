f774c8ac854a3fe5496bf475e5ffe21f854c466a
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2020-07-03 09:03:14 -0400

    Remove x2go support

--- CRDConf.py
+++ CRDConf.py
@@ -20,7 +20,6 @@ parser = argparse.ArgumentParser(description='Create synergy-core configuration.
 parser.add_argument("-n", "--noprompt", help='Do not prompt to continue.', action="store_true")
 parser.add_argument("-s", "--vncsd", help='Install vnc as a systemd service.', action="store_true")
 parser.add_argument("-v", "--x0vnc", help="Install vnc for the user's display.", action="store_true")
-parser.add_argument("-x", "--x2go", help="Install x2go.", action="store_true")
 
 # Save arguments.
 args = parser.parse_args()
@@ -36,7 +35,6 @@ print("Group Name is:", USERGROUP)
 # Print out configuration information
 print("VNC systemd service config flag is", args.vncsd)
 print("x0vnc config flag is", args.x0vnc)
-print("x2go config flag is", args.x2go)
 
 
 if args.noprompt is False:
@@ -63,7 +61,7 @@ if args.vncsd is True or args.x0vnc is True:
 VncXstartupFile = VncUserFolder + '/xstartup'
 OpenboxConfigFolder = USERHOME + "/.config/openbox"
 OpenboxConfigFile = OpenboxConfigFolder + "/autostart"
-if args.vncsd is True or args.x2go is True:
+if args.vncsd is True:
     with open(VncXstartupFile, 'w') as VncXstartupFile_handle:
         VncXstartupFile_handle.write("""#!/bin/bash
 unset SESSION_MANAGER
@@ -89,7 +87,7 @@ if args.vncsd is True or args.x0vnc is True:
     if not os.path.isfile(VncPasswordPath):
         # Loop until the password has been successfully saved.
         VncPassReturnCode = 1
-        while VncPassReturnCode is not 0:
+        while VncPassReturnCode != 0:
             print("Enter a VNC Password (stored in {0}).".format(VncPasswordPath))
             status = subprocess.run("vncpasswd {0}".format(VncPasswordPath), shell=True)
             VncPassReturnCode = status.returncode
@@ -148,15 +146,6 @@ TimeoutStopSec=7s
 WantedBy=default.target
 """.format(vncpath=shutil.which("x0vncserver"), vncpass=VncPasswordPath)
     CFunc.systemd_createuserunit("vncx0.service", X0VncUserUnitText)
-
-# Config for x2go
-if args.x2go is True:
-    print("Installing x2go.")
-    if shutil.which("dnf"):
-        CFunc.dnfinstall("x2goserver fuse-sshfs")
-    elif shutil.which("apt-get"):
-        CFunc.addppa("ppa:x2go/stable")
-        CFunc.aptinstall("x2goserver x2goserver-xsession sshfs")
 
 # Ensure ownerships are correct
 if os.path.isdir(VncUserFolder):
