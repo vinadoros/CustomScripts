3f4ce6bb6cc60e1106144af911c2d98ecbb91b50
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2020-07-15 08:01:08 -0400

    Make some logs normal

--- suspend-when-idle.py
+++ suspend-when-idle.py
@@ -27,7 +27,7 @@ args = parser.parse_args()
 if args.debug:
     log_level = logging.DEBUG
 else:
-    log_level = logging.CRITICAL
+    log_level = logging.INFO
 
 logging.basicConfig(level=log_level, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -55,14 +55,18 @@ def reset_timers():
 def check_idle():
     """Check if network services are not being used."""
     status = False
+    inhibit_string = ""
     # Get network information. Use the -n flag to speed up output, but lose the port names and instead must check using numbers.
     netstat_output = subprocess.check_output("netstat -tupan", shell=True)
     # Check ssh status
     ssh_status = grep_in_variable(netstat_output, r"ESTABLISHED.*sshd")
+    inhibit_string += "SSH: {0}, ".format(ssh_status)
     # Check samba status
     samba_status = grep_in_variable(netstat_output, r"ESTABLISHED.*smbd")
+    inhibit_string += "Samba: {0}, ".format(samba_status)
     # Check nfs status. NFS is usually served on port 2049.
     nfs_status = grep_in_variable(netstat_output, r":2049.*ESTABLISHED")
+    inhibit_string += "NFS: {0}, ".format(nfs_status)
     # Check libvirt status. Inhibit suspend if any VM is running.
     libvirt_status = False
     libvirt_lines = 0
@@ -71,14 +75,15 @@ def check_idle():
     # Libvirt outputs 1 line if no VMs are running. Will output 2 or more if VMs are running.
     if libvirt_lines >= 2:
         libvirt_status = True
-    logging.debug("Samba: %s, NFS: %s, SSH: %s, libvirt: %s", samba_status, nfs_status, ssh_status, libvirt_status)
+    inhibit_string += "libvirt: {0}".format(libvirt_status)
+    logging.info(inhibit_string)
     if samba_status is True or nfs_status is True or ssh_status is True or libvirt_status is True:
         status = True
     return status
 
 
 ### Begin Code ###
-logging.critical("Script Started")
+logging.info("Script Started")
 reset_timers()
 while True:
     # Check if services are being used.
@@ -89,9 +94,10 @@ while True:
     else:
         current_time = datetime.datetime.now()
     logging.debug("Before suspend, Current Time: %s, Suspend Time: %s", current_time, suspend_time)
+    logging.info("Minutes until suspend: %s", ((suspend_time - current_time).total_seconds() / 60))
     # Suspend if the current time exceeds the suspend time.
     if current_time >= suspend_time:
-        logging.critical("Suspending.")
+        logging.info("Suspending.")
         # Suspend the system.
         # subprocess.run("systemctl suspend -i", shell=True, check=True)
         subprocess.run("systemctl start systemd-suspend.service", shell=True, check=True)
@@ -99,8 +105,8 @@ while True:
         time.sleep(20)
         # If a suspend occurs, reset the timers to the startup values, and begin counting down again.
         reset_timers()
-        logging.critical("Came out of suspend.")
+        logging.info("Came out of suspend.")
     # Wait one minute before checking the services again.
     time.sleep(60)
 
-logging.critical("Script Exited")
+logging.info("Script Exited")
