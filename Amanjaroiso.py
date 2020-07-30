12c1d56cb784f8fccd8a8b867262f2d59af1f267
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2020-07-30 09:27:31 -0400

    Enable ssh for manjaroiso

--- Amanjaroiso.py
+++ Amanjaroiso.py
@@ -61,7 +61,17 @@ CFunc.pacman_install("nano git lsb-release manjaro-tools-iso")
 
 # Clone the iso-profiles repository.
 CFunc.gitclone("https://gitlab.manjaro.org/profiles-and-settings/iso-profiles.git", os.path.join(USERHOME, "iso-profiles"))
+# Build chroot
 subprocess.run("buildiso -p xfce -b stable -x -r '{0}' -t '{1}'".format(workfolder, outputfolder), shell=True, check=False)
+
 # Changes
+# Enable sshd
+os.makedirs("{0}/xfce/x86_64/desktopfs/etc/systemd/system/multi-user.target.wants/".format(workfolder), exist_ok=True)
+subprocess.run("ln -sfr {0}/xfce/x86_64/desktopfs/usr/lib/systemd/system/sshd.service {0}/xfce/x86_64/desktopfs/etc/systemd/system/multi-user.target.wants/sshd.service".format(workfolder), shell=True, check=True)
+# Allow root login for ssh
+subprocess.run("sed -i 's/PermitRootLogin.*/PermitRootLogin yes/g' {0}/xfce/x86_64/desktopfs/etc/ssh/sshd_config".format(workfolder), shell=True, check=True)
+subprocess.run("sed -i '/^#PermitRootLogin.*/s/^#//g' {0}/xfce/x86_64/desktopfs/etc/ssh/sshd_config".format(workfolder), shell=True, check=True)
+
+# Build the ISO
 subprocess.run("buildiso -p xfce -cz -r '{0}' -t '{1}'".format(workfolder, outputfolder), shell=True, check=False)
 # TODO: Insert check to make sure the ISO built properly.
