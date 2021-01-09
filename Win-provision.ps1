1c0cc3f1f07628e6317851cdac2a9add43f2ae9b
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2021-01-09 11:29:30 -0500

    Remove libreoffice

--- Win-provision.ps1
+++ Win-provision.ps1
@@ -203,7 +203,7 @@ function Fcn-Software {
   if ( $core -eq $false ) {
     Write-Output "Installing Desktop Apps"
     # GUI Apps
-    choco upgrade -y firefox notepadplusplus tortoisegit bleachbit putty chocolateygui conemu microsoft-windows-terminal VisualStudioCode libreoffice-fresh sumatrapdf WizTree
+    choco upgrade -y firefox notepadplusplus tortoisegit bleachbit putty chocolateygui conemu microsoft-windows-terminal VisualStudioCode sumatrapdf WizTree
     choco upgrade -y open-shell ShutUp10
     # Set default browser.
     choco upgrade -y setdefaultbrowser
