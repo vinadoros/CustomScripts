facd0ed92ddb930702671b4d282e219e68f7c8aa
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2021-02-27 15:35:58 -0500

    Change firefox homepage

--- Dset.py
+++ Dset.py
@@ -476,6 +476,7 @@ if os.path.isdir(firefox_profiles_path):
                     firefox_modify_settings("app.shield.optoutstudies.enabled", "false", prefsjs_file)
                     firefox_modify_settings("browser.newtabpage.activity-stream.showSponsored", "false", prefsjs_file)
                     firefox_modify_settings("browser.newtabpage.enabled", "false", prefsjs_file)
+                    firefox_modify_settings("browser.startup.homepage", "about:blank", prefsjs_file)
                     # DNS-over-HTTPS
                     firefox_modify_settings("network.trr.mode", "2", prefsjs_file)
                     firefox_modify_settings("network.trr.bootstrapAddress", '"1.1.1.1"', prefsjs_file)
